a78fc8e509802337e3d39c7cf28d0e6f99e13c79
Moran <method.even@gmail.com>
2023-02-01 12:08:34 -0800

    controls

--- controls.js
+++ controls.js
@@ -1,3 +1,41 @@
 class Controls {
-    
+    constructor() {
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
+        this.#addKeyboardListeners()
+    }
+
+    #addKeyboardListeners() {
+        document.onkeydown=e=>{
+            switch(e.key){
+                case 'ArrowLeft':
+                    this.left=true;
+                case 'ArrowRight':
+                    this.right=true;
+                case 'ArrowDown':
+                    this.reverse=true;
+                case 'ArrowUp':
+                    this.forward=true;
+                    break;
+            }
+            console.table(this)
+        }
+        document.onkeyup = (e) => {
+          switch (e.key) {
+            case "ArrowLeft":
+              this.left = false;
+            case "ArrowRight":
+              this.right = false;
+            case "ArrowDown":
+              this.reverse = false;
+            case "ArrowUp":
+              this.forward = false;
+              break;
+          }
+          console.table(this);
+        };
+    }
 }
